1e177e8ecb3a8eb08c6445dd06726cc32fd61c1f
Goumba <39564795+Goumba@users.noreply.github.com>
2019-02-22 13:35:58 -0500

    septieme commit

--- module_js/application.js
+++ module_js/application.js
@@ -2,22 +2,25 @@ console.log('application');
 import {contenuIntro} from './contenuIntro.js' // le contenu de l'intoduction */
 import {Introduction} from './Introduction.js' //
 import {AnimLettre} from './AnimLettre.js' //
+import {AnimDeux} from './AnimDeux.js' //
 
 
 /* l'élement de la page qui contiendra les éléments créés dynamiquement */
 let elmHeader = document.querySelector('.titre')
-//let intro = new Introduction(contenuIntro, elmHeader, animationLettre)
 const lesLettres = 'Gros Robot'
+let intro = new Introduction(contenuIntro, elmHeader, animationLettre)
 let  monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim);
 
 // debutQuestionnaire()
 
 function animationLettre()
 {
+	
     /* Une fois que l'animation des mots est terminé la fonction animLettre s'exécutera */ 
 	console.log('debut animation lettre')
-	const lesLettres = 'Big Bad Robot'
-	let  monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim);	
+	const lesLettres = 'Très Gros Robot'
+	let  monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim);
+	let  monAnimDeux = new AnimDeux(lesLettres, elmHeader, finAnim);	
 }
 
 
